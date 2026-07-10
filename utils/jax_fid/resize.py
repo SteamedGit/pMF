0059728649ced7b5858d60dddda6b608b93580c3
Timothy Hitge <timothyhitge@gmail.com>
2026-07-10 17:56:54 +0200

    resize.forward was missing, got it from iMF

--- utils/jax_fid/resize.py
+++ utils/jax_fid/resize.py
@@ -2,6 +2,10 @@ from __future__ import annotations
 
 import jax.numpy as jnp
 
+import torch
+from torch import Tensor
+
+
 def resize_torch_grid_sample(x, out_h=299, out_w=299):
     assert x.ndim == 4, x.shape
     B, H, W, C = x.shape
@@ -14,7 +18,7 @@ def resize_torch_grid_sample(x, out_h=299, out_w=299):
 
     row0 = jnp.take(x, y0, axis=1, mode="clip")
     row1 = jnp.take(x, y1, axis=1, mode="clip")
-    tmp  = row0 * (1.0 - wy) + row1 * wy
+    tmp = row0 * (1.0 - wy) + row1 * wy
 
     xs = (jnp.arange(out_w, dtype=jnp.float32) + 0.5) * (W / out_w) - 0.5
     x0 = jnp.floor(xs).astype(jnp.int32)
@@ -23,6 +27,51 @@ def resize_torch_grid_sample(x, out_h=299, out_w=299):
 
     col0 = jnp.take(tmp, x0, axis=2, mode="clip")
     col1 = jnp.take(tmp, x1, axis=2, mode="clip")
-    out  = col0 * (1.0 - wx) + col1 * wx
-    
+    out = col0 * (1.0 - wx) + col1 * wx
+
     return out
+
+
+# was missing from pMF got it from: https://github.com/Lyy-iiis/imeanflow/blob/main/utils/jax_fid/resize.py
+def forward(
+    img: Tensor,
+) -> Tensor:
+
+    _0 = torch.nn.functional.affine_grid
+    _1 = torch.nn.functional.grid_sample
+    (
+        batch_size,
+        channels,
+        height,
+        width,
+    ) = img.shape
+    x = img
+    theta = torch.eye(2, 3, dtype=None, layout=None)
+    _3 = torch.select(torch.select(theta, 0, 0), 0, 2)
+    _4 = torch.select(torch.select(theta, 0, 0), 0, 0)
+    _5 = torch.div(_4, width)
+    _6 = torch.select(torch.select(theta, 0, 0), 0, 0)
+    _7 = torch.add(_3, torch.sub(_5, torch.div(_6, 299)))
+    _8 = torch.select(torch.select(theta, 0, 1), 0, 2)
+    _9 = torch.select(torch.select(theta, 0, 1), 0, 1)
+    _10 = torch.div(_9, height)
+    _11 = torch.select(torch.select(theta, 0, 1), 0, 1)
+    _12 = torch.add(_8, torch.sub(_10, torch.div(_11, 299)))
+    _13 = torch.unsqueeze(theta, 0)
+    theta0 = _13.repeat([batch_size, 1, 1])
+    grid = _0(
+        theta0,
+        [batch_size, channels, 299, 299],
+        False,
+    )
+    x0 = _1(
+        x,
+        grid,
+        "bilinear",
+        "border",
+        False,
+    )
+    x1 = torch.sub(x0, 128)
+    x2 = torch.div(x1, 128)
+
+    return x2
